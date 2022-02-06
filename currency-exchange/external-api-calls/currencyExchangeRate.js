54ba519d577837ca057fdffe28c1c74a24ec5b21
unknown <jpscriven@live.co.za>
2022-02-06 10:22:56 +0200

    fix currencies url append

--- currency-exchange/external-api-calls/currencyExchangeRate.js
+++ currency-exchange/external-api-calls/currencyExchangeRate.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 async function getCurrencyExchangeRate(localCurrency){
     const FOREX_API_KEY = process.env.FOREX_API_KEY;
-    const API_URL = `http://api.currencylayer.com/live?format=1&access_key=${FOREX_API_KEY}&${localCurrency}`;
+    const API_URL = `http://api.currencylayer.com/live?format=1&access_key=${FOREX_API_KEY}&currencies=${localCurrency}`;
     const promise = axios.get(API_URL)
     const currencyConvertTo = localCurrency;
     const currencyConvertFrom = 'USD';
